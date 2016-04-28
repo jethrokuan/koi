691184aaec56c82c65e669646b3b60432d968b02
Jethro Kuan <jethrokuan95@gmail.com>
2016-04-28 18:28:14 +0800

    tidy up code

--- functions/koi.fish
+++ functions/koi.fish
@@ -1,89 +1,111 @@
 function koi -d "Create a plugin from a template" -a template name
-  set -l color (set_color $fish_color_end)
-set -l color_normal (set_color normal)
-set -l url
+  set -l url
 
-switch "$template"
-  case -h
-    printf "Usage: koi <url> <name> [--help]\n\n"
-    printf "    -h --help    Show usage help\n"
-    printf "Example:\n"
-    printf "koi jethrokuan/koi-template foo\n"
-    return
+  function __koi_report -a level message
+    set -l string
+    set -l color_koi (set_color 1693A5)
+    set -l color_success (set_color green)
+    set -l color_warn (set_color yellow)
+    set -l color_error (set_color red)
+    set -l color_normal (set_color normal)
+    switch "$level"
+      case "success"
+        set string "$color_koi koi $color_success SUCCESS $color_normal $message\n"
+      case "warn"
+        set string "$color_koi koi $color_warn WARN $color_normal    $message\n"
+      case "error"
+        set string "$color_koi koi $color_error ERROR $color_normal   $message\n"
+      case \*
+        return 1
+    end
+    printf "$string"
+  end
 
-  case \*/\*
-    set url "https://github.com/$template"
+  switch "$template"
+    case -h
+      printf "Usage: koi <url> <name> [--help]\n\n"
+      printf "    -h --help    Show usage help\n"
+      printf "Example:\n"
+      printf "koi jethrokuan/koi-template foo\n"
+      return
 
-  case https://\?\*
-    set url "$template"
+    case \*/\*
+      set url "https://github.com/$template"
 
-  case \*
-    printf "koi: '%s' is not a valid template.\n\n" $template > /dev/stderr
-    return 1
-end
+    case https://\?\*
+      set url "$template"
 
-if not set -q name
-  get --prompt "Project name:\t" | read -l name
-end
+    case \*
+      __koi_report "error" "koi: '$template' is not a valid template."
+      return 1
+  end
 
-if test -d "$name"
-  printf "koi: '%s' already exists and is not empty." $name > /dev/stderr
-  return 1
-else
-  printf "Cloning $url into '$name'...\n"
-  fish -c "command git clone -q $url $name & await"
+  if not set -q name
+    get --prompt "Project name:\t" | read -l name
+  end
 
-  # Very rough check on success of git clone
   if test -d "$name"
-    printf "    ✓ Cloned successfully\n" $name > /dev/stderr
-  else
-    printf "    × Clone unsucessful\n" $name > /dev/stderr
+    __koi_report "error" "koi: '$name' already exists and is not empty."
     return 1
-  end
-end
+  else
+    printf "Cloning $url into '$name'...\n"
+    fish -c "command git clone -q $url $name & await"
 
-pushd "$name"
-
-function __koi_cleanup -d "Cleans up after installation"
-  if test -e .vars
-    rm .vars > /dev/null
-  end
-  rm -rf .git > /dev/null
-  printf "\nkoi: project successfully created!\n"
-  functions -e __koi_cleanup
-end
-
-# Read vars from input
-set -l vars
-if test -s .vars
-  set arrayName (cat .vars)
-  for var in $arrayName
-    set vars $vars $var
-  end
-else
-  # No variables, work is done
-  __koi_cleanup
-  return 0
-end
-
-printf "\n Reading variable file:\n"
-
-set -l input
-for var in $vars
-  set -l promptvar
-  get --prompt "   $var: " | read -l promptvar
-  set input $input $promptvar
-end
-
-for file in **
-  if test -f "$file"
-    for i in (seq (count $vars))
-      sed -i.tmp "s|{{{$vars[$i]}}}|$input[$i]|g" "$file"
-      command rm -f "$file.tmp"
+    # Very rough check on success of git clone
+    if test -d "$name"
+      __koi_report "success" "Clone successful"
+    else
+      __koi_report "error" "Clone unsucessful"
+      return 1
     end
   end
-end
 
-__koi_cleanup
-popd
+  pushd "$name"
+
+  function __koi_cleanup -d "Cleans up after installation"
+    if test -e .vars
+      rm .vars > /dev/null
+    end
+    rm -rf .git > /dev/null
+    popd
+    __koi_report "success" "project successfully created!"
+    functions -e __koi_cleanup
+  end
+
+  # Read vars from input
+  set -l vars
+  if test -s .vars 
+    set arrayName (cat .vars)
+    for var in $arrayName
+      set vars $vars $var
+    end
+  else
+    __koi_report "warn" "No variables to read from template"
+    # No variables, work is done
+    __koi_cleanup
+    return 0
+  end
+
+  printf "\n Reading variable file:\n"
+
+  set -l input
+  for var in $vars
+    set -l promptvar
+    get --prompt " $var:" | read -l promptvar
+    set input $input $promptvar
+  end
+
+  printf "\n"
+  
+  for file in **
+    if test -f "$file"
+      for i in (seq (count $vars))
+        sed -i.tmp "s|{{{$vars[$i]}}}|$input[$i]|g" "$file"
+        command rm -f "$file.tmp"
+      end
+    end
+  end
+
+  __koi_cleanup
+  popd
 end
